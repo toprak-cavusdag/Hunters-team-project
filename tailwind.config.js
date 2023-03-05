20b20ab56dc2200942527d7d5b91f9c8d22f3fc9
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-03-05 14:46:56 +0300

    Hunters Team first push

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.{html,js}'],
+  theme: {
+    extend: {},
+    colors: {
+      'Hunter-color': '#db5416',
+      'Hunter-black': '#000',
+      'text-white': '#fff',
+      'gray-500': '#64748b',
+    },
+  },
+  plugins: [],
+};
