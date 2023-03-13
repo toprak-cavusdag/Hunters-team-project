b84872ace9292c1449e133430252c7450d7c8167
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-03-13 14:13:12 +0300

    same changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.{html,js}'],
+  content: ['./dist/*.{html,js}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
     colors: {
